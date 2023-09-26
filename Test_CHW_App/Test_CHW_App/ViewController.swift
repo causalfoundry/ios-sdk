0d7a9198c8382467757f5c3fcdd2d1e7ac94fa8a
khushbu patel <khushbur64.patel@gmail.com>
2023-09-26 17:13:30 -0400

    Code transfer
    initialize

---
+++ Test_CHW_App/Test_CHW_App/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Test_CHW_App
+//
+//  Created by khushbu on 10/09/23.
+//
+
+import UIKit
+import CHW_SDK
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
