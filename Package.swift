27ffff4da9df3aa20325b596de5c78e022188b9a
khushbu patel <khushbur64.patel@gmail.com>
2023-09-29 12:35:22 -0400

    Change related to Build settings

--- Package.swift
+++ Package.swift
@@ -4,25 +4,19 @@
 import PackageDescription
 
 let package = Package(
-    name: "CausalFoundry",
+    name: "CausalFoundry_ios_SDK",
     platforms: [.iOS(.v13)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "CausalFoundry",
-            targets: ["CausalFoundry"]),
-        
-    ],
-   
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CausalFoundry"
+            name: "CausalFoundry",
+            path: "Core/Sources/Core",
+            publicHeadersPath: "./"
         ),
-        .testTarget(
-            name: "CausalFoundryTests",
-            dependencies: ["CausalFoundry"]),
+//        .testTarget(
+//            name: "CausalFoundryTests",
+//            dependencies: ["CausalFoundry"]),
        
     ]
 )
