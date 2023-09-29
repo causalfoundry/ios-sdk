1606600f583ef4b8d17be814179b3ac8ef35a903
khushbu patel <khushbur64.patel@gmail.com>
2023-09-29 17:42:50 -0400

    Chnage related to issue solving of path

--- Package.swift
+++ Package.swift
@@ -1,21 +1,36 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+//import PackageDescription
+//
+//let package = Package(
+//    name: "CausalFoundry_ios_SDK",
+//    platforms: [.iOS(.v13)],
+//    targets: [
+//        // Targets are the basic building blocks of a package, defining a module or a test suite.
+//        // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "CausalFoundry",
+//            path: "Core/Sources")
+//       // ,
+////        .testTarget(
+////            name: "CoreTests",
+////            dependencies: ["Core"]),
+//       
+//    ]
+//)
+
 import PackageDescription
 
 let package = Package(
     name: "CausalFoundry_ios_SDK",
     platforms: [.iOS(.v13)],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CausalFoundry",
-            path: "Core/Sources")
-       // ,
-//        .testTarget(
-//            name: "CoreTests",
-//            dependencies: ["Core"]),
-       
-    ]
+           // Targets are the basic building blocks of a package, defining a module or a test suite.
+           // Targets can depend on other targets in this package and products from dependencies.
+           .testTarget(
+               name: "CoreTests",
+               dependencies: ["CausalFoundry_ios_SDK"]),
+   
+       ]
 )
