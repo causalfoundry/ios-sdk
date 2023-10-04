ccc465408f336d32118975489e9eb5678d245ea8
khushbu patel <khushbur64.patel@gmail.com>
2023-10-04 15:55:54 -0400

    Chnage related to access level

--- Core/Sources/Services/APIHandler/AppLifecycleObserver.swift
+++ Core/Sources/Services/APIHandler/AppLifecycleObserver.swift
@@ -16,6 +16,7 @@ public final class appLifecycleobserver:NSObject,UIApplicationDelegate {
     {
         super.init()
         self.application = application
+        self.application?.delegate = self
        
     }
     
--- SampleApp/NewTestApp/AppDelegate.swift
+++ SampleApp/NewTestApp/AppDelegate.swift
@@ -11,11 +11,13 @@ import CasualFoundryCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        appLifecycleobserver(application: application)
-        
+        appLifecycleobserver(application: application).application?.delegate = self
+     
 //        CFLogBuilder(application: application)
 //                    .setSdkKey(sdkKey:"cfkey4dxUm8RIJmWmxgY4uakWFXqd1KmNk4Y14uHb0ogvqPpkJiGwEaKge4iGXAg")
 //                    .setAppLevelContentBlock(contentBlock: .core)
@@ -37,24 +39,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationDidEnterBackground(_ application: UIApplication) {
-        CFLogAppEventBuilder().setAppEvent(appAction: .background)
-        CFLogAppEventBuilder().build()
+//        CFLogAppEventBuilder().setAppEvent(appAction: .background)
+//        CFLogAppEventBuilder().build()
 
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    }
     
     
 
