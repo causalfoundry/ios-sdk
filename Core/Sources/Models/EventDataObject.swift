8aa78549000f06d9451b01f4f2e2c344d11c3adc
khushbu patel <khushbur64.patel@gmail.com>
2023-10-05 20:51:14 -0400

    Commnted props encoding

--- Core/Sources/Models/EventDataObject.swift
+++ Core/Sources/Models/EventDataObject.swift
@@ -35,21 +35,30 @@ struct EventDataObject: Codable {
 
     init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-//        block = try values.decodeIfPresent(String.self, forKey: .block)
-//        if let decodeAppobject =  try values.decodeIfPresent(AppObject.self, forKey: .props) {
-//            props = decodeAppobject
-//        }else if let decodeIndentityObject = try values.decodeIfPresent(IdentifyObject.self, forKey: .props) {
-//            props = decodeIndentityObject
-//        }else if let decodePropsObject = try values.decodeIfPresent(Props.self, forKey: .props) {
-//            props = decodePropsObject
-//        }else {
+        block = try values.decodeIfPresent(String.self, forKey: .block)
+        if let decodeAppobject =  try values.decodeIfPresent(AppObject.self, forKey: .props) {
+            props = decodeAppobject
+        }else if let decodeIndentityObject = try values.decodeIfPresent(IdentifyObject.self, forKey: .props) {
+            props = decodeIndentityObject
+        }else if let decodePropsObject = try values.decodeIfPresent(Props.self, forKey: .props) {
+            props = decodePropsObject
+        }else {
             props = nil
-       // }
+        }
         type = try values.decodeIfPresent(String.self, forKey: .type)
         ol = try values.decodeIfPresent(Bool.self, forKey: .ol)
         ts = try values.decodeIfPresent(String.self, forKey: .ts)
     }
     
+    // MARK: Encodable
+    func encode(to encoder: Encoder) throws {
+        var baseContainer = encoder.container(keyedBy: CodingKeys.self)
+        try baseContainer.encode(self.block, forKey: .block)
+       // try baseContainer.encode(self.props, forKey: .props)
+        try baseContainer.encode(self.type, forKey: .type)
+        try baseContainer.encode(self.ol, forKey: .ol)
+        try baseContainer.encode(self.ts, forKey: .ts)
+    }
     
 
 }
@@ -98,9 +107,6 @@ extension EventDataObject {
         
     }
     
-    public func encode(to encoder: Encoder) throws {
-        
-   
-    }
+    
 }
 
