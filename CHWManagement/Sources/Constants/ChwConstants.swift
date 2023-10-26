c68c3ff359c3a905fe973973c97b4ea5e7db2bb4
khushbu patel <khushbur64.patel@gmail.com>
2023-10-26 12:09:11 -0400

    Chnages related to error solving

--- CHWManagement/Sources/Constants/ChwConstants.swift
+++ CHWManagement/Sources/Constants/ChwConstants.swift
@@ -12,24 +12,25 @@ import CasualFoundryCore
 enum ChwConstants {
     static var contentBlockName: String = ContentBlock.chw_mgmt.rawValue
 
-    static func verifyChwCatalog(_ chwId: String, _ chwCatalogModel: ChwCatalogModel) -> InternalChwModel {
+    static func verifyChwCatalog(_ chwId: String, _ chwCatalogModel: ChwCatalogModel) -> InternalChwModel? {
         let catalogName = CatalogSubject.chw.rawValue + " catalog"
 
         guard !chwId.isEmpty else {
             ExceptionManager.throwIsRequiredException(eventType: catalogName, elementName: "Chw Id")
-            
+            return nil
         }
 
         for item in chwCatalogModel.rolePermissions {
             guard !item.isEmpty else {
                 ExceptionManager.throwRuntimeException(eventType: catalogName, message: "Invalid role_permissions provided")
-                
+                return nil
             }
         }
 
         for item in chwCatalogModel.siteIdsList {
             guard !item.isEmpty else {
                  ExceptionManager.throwRuntimeException(eventType: catalogName, message: "Invalid site_id_list provided")
+                return nil
                 
             }
         }
@@ -37,6 +38,7 @@ enum ChwConstants {
         for item in chwCatalogModel.servicesList {
             guard !item.isEmpty else {
                  ExceptionManager.throwRuntimeException(eventType: catalogName, message: "Invalid services provided")
+                return nil
                 
             }
         }
@@ -52,17 +54,19 @@ enum ChwConstants {
         )
     }
 
-    static func verifySiteCatalog(_ siteId: String, _ chwSiteCatalogModel: ChwSiteCatalogModel) throws -> InternalSiteModel {
+    static func verifySiteCatalog(_ siteId: String, _ chwSiteCatalogModel: ChwSiteCatalogModel) throws -> InternalSiteModel? {
         let catalogName = CatalogSubject.chwsite.rawValue + " catalog"
         
         guard !siteId.isEmpty else {
             ExceptionManager.throwIsRequiredException(eventType: catalogName, elementName: "Site Id")
+            return nil
             
         }
         
         if !chwSiteCatalogModel.country.isEmpty {
             guard let countryCode = CountryCode(rawValue: chwSiteCatalogModel.country) else {
                 ExceptionManager.throwEnumException(eventType: catalogName, className: "CountryCode")
+                return nil
                 
             }
         }
@@ -86,17 +90,19 @@ enum ChwConstants {
         
     }
 
-    static func verifyPatientCatalog(_ patientId: String, _ patientCatalogModel: PatientCatalogModel) throws -> InternalPatientModel {
+    static func verifyPatientCatalog(_ patientId: String, _ patientCatalogModel: PatientCatalogModel) throws -> InternalPatientModel? {
         let catalogName = CatalogSubject.patient.rawValue + " catalog"
 
         guard !patientId.isEmpty else {
              ExceptionManager.throwIsRequiredException(eventType:catalogName , elementName: "Patient Id")
+            return nil
             
         }
 
         if !patientCatalogModel.country.isEmpty {
             guard let countryCode = CountryCode(rawValue: patientCatalogModel.country) else {
                  ExceptionManager.throwEnumException(eventType:catalogName , className:"CountryCode")
+                return nil
                 
             }
         }
@@ -104,6 +110,7 @@ enum ChwConstants {
         if !patientCatalogModel.educationLevel.isEmpty {
             guard let educationalLevel = EducationalLevel(rawValue: patientCatalogModel.educationLevel) else {
                  ExceptionManager.throwEnumException(eventType:catalogName , className: "EducationalLevel")
+                return nil
                 
             }
         }
@@ -111,6 +118,7 @@ enum ChwConstants {
         for item in patientCatalogModel.siteIdsList {
             guard !item.isEmpty else {
                  ExceptionManager.throwRuntimeException(eventType: catalogName, message: "Invalid site_ids_list provided")
+                return nil
                 
             }
         }
