754e269abe81905c443a56db1c0b9c1caae8239a
khushbu patel <khushbur64.patel@gmail.com>
2023-10-26 14:26:34 -0400

    Chnages related to access level

--- Core/Sources/Models/ChwModelObject.swift
+++ Core/Sources/Models/ChwModelObject.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct ChwModelObject: Codable {
+public struct ChwModelObject: Codable {
     var type: String
     var meta: Any?
 
@@ -18,14 +18,14 @@ struct ChwModelObject: Codable {
     }
 
    
-    func encode(to encoder: Encoder) throws {
+    public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(type, forKey: .type)
         try container.encode(meta as? String, forKey: .meta)
     }
 
     // Decoding
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         type = try container.decode(String.self, forKey: .type)
         meta = try container.decodeIfPresent(String.self, forKey: .meta)
