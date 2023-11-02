fd233ab39fe221adbd9fae676d0ce6fd86434522
khushbu patel <khushbur64.patel@gmail.com>
2023-11-02 08:55:36 -0400

    Chaanges related to resource access

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CausalFoundry_ios_SDK",
     platforms: [.iOS(.v13)],
-  
+    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -24,20 +24,23 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CasualFoundryCore",
-            path: "Core/Sources"),
-        .target(
-            name: "CasualFoundryCHWManagement",
-            dependencies:["CasualFoundryCore"], path: "CHWManagement/Sources"),
+            path: "Core/Sources",
+            resources: [
+            .copy("Resources/usd_rates.json"),
+            ]),
+            .target(
+                name: "CasualFoundryCHWManagement",
+                dependencies:["CasualFoundryCore"], path: "CHWManagement/Sources"),
         .target(
             name: "CasualFoundryEcommerce",
             dependencies:["CasualFoundryCore"], path: "E_Commerce/Sources"
-           
+            
         ),
-       
-        .target(
-            name: "CoreTests",
-            dependencies: ["CasualFoundryCore"],
-            path: "Core/Tests"),
+        
+            .target(
+                name: "CoreTests",
+                dependencies: ["CasualFoundryCore"],
+                path: "Core/Tests"),
         .testTarget(
             name: "CHWManagementTests",
             dependencies: ["CasualFoundryCHWManagement"],
