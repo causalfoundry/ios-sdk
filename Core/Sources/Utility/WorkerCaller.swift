4fe6f4aadd43806a0f29057bc6ecaeb4fc3221fd
khushbu patel <khushbur64.patel@gmail.com>
2023-11-10 11:55:54 -0500

    Chnages related to API Calling

--- Core/Sources/Utility/WorkerCaller.swift
+++ Core/Sources/Utility/WorkerCaller.swift
@@ -13,70 +13,15 @@ class WorkerCaller {
     var backgroundTaskIdentifier = "com.causalFoundry.updateAppEvents"
     
     static func registerBackgroundTask() {
-        // Check if the app supports background fetch
+        
         BGTaskScheduler.shared.register(forTaskWithIdentifier:WorkerCaller().backgroundTaskIdentifier, using: nil) { task in
-            // Perform the work inside the background task
-            self.handleAppRefresh(task: task as! BGProcessingTask)
-        }
-        
-    }
-    
-    
-    static func updateAppEvents(application:UIApplication) {
-////        
-////        BGTaskScheduler.shared.register(forTaskWithIdentifier: WorkerCaller().backgroundTaskIdentifier, using: nil) { task in
-////            // Perform the work inside the background task
-////            self.handleAppRefresh(task: task as! BGAppRefreshTask)
-////        }
-//        
-//        // Create a background task request
-//        let request = BGAppRefreshTaskRequest(identifier: WorkerCaller().backgroundTaskIdentifier)
-//        request.earliestBeginDate = Date()
-//        
-//        // Schedule the background task
-//        do {
-//            try BGTaskScheduler.shared.submit(request)
-//        } catch {
-//            // Handle any errors when scheduling the task
-//        }
-        
-    }
-    
-    static func handleAppRefresh(task: BGProcessingTask) {
-        // Schedule the next background refresh
-        self.scheduleNextAppRefresh()
-        
-        // Create a queue to perform the background task
-        let backgroundQueue = DispatchQueue.global(qos: .background)
-        
-        // Start the background task
-        task.expirationHandler = {
-            // Handle expiration if needed
-        }
-        
-        backgroundQueue.async {
-            // Perform API calls
-            self.performAPICalls()
-            
-            // Mark the task as completed
-            task.setTaskCompleted(success: true)
+           self.handleBackgroundTask(task: task as! BGProcessingTask)
         }
     }
-    
-    static func scheduleNextAppRefresh() {
-        // Calculate the next time to perform the background refresh
-        let nextRefreshTime = Calendar.current.date(byAdding: .minute, value: 15, to: Date())
-        
-        // Create a new background task request
-        let request = BGAppRefreshTaskRequest(identifier: "com.causalFoundry.updateAppEvents")
-        request.earliestBeginDate = nextRefreshTime
-        
-        // Submit the task request to the system
-        do {
-            try BGTaskScheduler.shared.submit(request)
-        } catch {
-            print("Error scheduling background task: \(error.localizedDescription)")
-        }
+
+    static func handleBackgroundTask(task: BGProcessingTask) {
+        self.performAPICalls()
+        task.setTaskCompleted(success: true)
     }
     
     static  func performAPICalls() {
