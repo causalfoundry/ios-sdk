3057404b6d869e2aae69a93d7b404f41f25b1293
khushbu patel <khushbur64.patel@gmail.com>
2023-11-15 13:45:17 -0500

    Chnages related to background task

--- Core/Sources/Utility/WorkerCaller.swift
+++ Core/Sources/Utility/WorkerCaller.swift
@@ -11,29 +11,44 @@ import UIKit
 public class WorkerCaller {
     // Method to update events at session end
    public var backgroundTaskIdentifier = "com.causalFoundry.updateAppEvents"
+    var backgroundTask: UIBackgroundTaskIdentifier = .invalid
     
     
     func scheduleBackgroundTask() {
-            // Register the background task
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: WorkerCaller().backgroundTaskIdentifier, using: nil) { task in
-                // Perform the upload task
-            print("Background task handler called...")
-            WorkerCaller.handleBackgroundTask(task: task as! BGProcessingTask)
-        }
+//            // Register the background task
+//        BGTaskScheduler.shared.register(forTaskWithIdentifier: WorkerCaller().backgroundTaskIdentifier, using: nil) { task in
+//                // Perform the upload task
+//            print("Background task handler called...")
+//            WorkerCaller.handleBackgroundTask(task: task as! BGProcessingTask)
+//        }
+//
+//            // Create and schedule the background task request
+//            let request = BGProcessingTaskRequest(identifier: WorkerCaller().backgroundTaskIdentifier)
+//            request.requiresNetworkConnectivity = true // Set as needed
+//            request.requiresExternalPower = false // Set as needed
+//
+//            do {
+//                try BGTaskScheduler.shared.submit(request)
+//            } catch {
+//                print("Unable to schedule background task: \(error)")
+//            }
+            backgroundTask = UIApplication.shared.beginBackgroundTask { [weak self] in
+                  self?.endBackgroundTask()
+              }
 
-            // Create and schedule the background task request
-            let request = BGProcessingTaskRequest(identifier: WorkerCaller().backgroundTaskIdentifier)
-            request.requiresNetworkConnectivity = true // Set as needed
-            request.requiresExternalPower = false // Set as needed
+              DispatchQueue.global(qos: .background).async {
+                  // Your long-running API call logic goes here
+                  WorkerCaller.performAPICalls()
 
-            do {
-                try BGTaskScheduler.shared.submit(request)
-            } catch {
-                print("Unable to schedule background task: \(error)")
-            }
+                  // Make sure to call endBackgroundTask when your task is completed
+                  
+              }
         }
     
-    
+    func endBackgroundTask() {
+           UIApplication.shared.endBackgroundTask(backgroundTask)
+           backgroundTask = .invalid
+       }
    
     public static func handleBackgroundTask(task: BGProcessingTask) {
         print("Handling background task...")
@@ -48,11 +63,7 @@ public class WorkerCaller {
                let result2: () = try await ExceptionEventsUploader.uploadEvents()
                 
                 
-                // Process the API responses
-                print("API Response 1: \(result1)")
-                print("API Response 2: \(result2)")
-                
-                // Continue with any further processing...
+              
             } catch {
                 // Handle errors
                 print("Error: \(error)")
