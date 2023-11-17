dcab1c76e605a83e3396e962d28568d9d613af6d
khushbu patel <khushbur64.patel@gmail.com>
2023-11-17 15:16:29 -0500

    Chnages related to update catalog

--- E_Commerce/Sources/Catalog/CfEComCatalog.swift
+++ E_Commerce/Sources/Catalog/CfEComCatalog.swift
@@ -20,23 +20,23 @@ public class CfEComCatalog {
             }
         case ItemType.blood.rawValue:
             if let bloodCatalogModel = catalogModel as? BloodCatalogModel {
-                CfEComCatalog().updateBloodCatalog(itemId: itemId, bloodCatalogModel: bloodCatalogModel)
+                CfEComCatalog.updateBloodCatalog(itemId: itemId, bloodCatalogModel: bloodCatalogModel)
             }
         case ItemType.oxygen.rawValue:
             if let oxygenCatalogModel = catalogModel as? OxygenCatalogModel {
-                CfEComCatalog().updateOxygenCatalog(itemId: itemId, oxygenCatalogModel: oxygenCatalogModel)
+                CfEComCatalog.updateOxygenCatalog(itemId: itemId, oxygenCatalogModel: oxygenCatalogModel)
             }
         case ItemType.medicalEquipment.rawValue:
             if let medicalEquipmentCatalogModel = catalogModel as? MedicalEquipmentCatalogModel {
-                CfEComCatalog().updateMedicalEquipmentCatalog(itemId: itemId, medicalEquipmentCatalogModel: medicalEquipmentCatalogModel)
+                CfEComCatalog.updateMedicalEquipmentCatalog(itemId: itemId, medicalEquipmentCatalogModel: medicalEquipmentCatalogModel)
             }
         case ItemType.grocery.rawValue:
             if let groceryCatalogModel = catalogModel as? GroceryCatalogModel {
-                CfEComCatalog().updateGroceryCatalog(itemId: itemId, groceryCatalogModel: groceryCatalogModel)
+                CfEComCatalog.updateGroceryCatalog(itemId: itemId, groceryCatalogModel: groceryCatalogModel)
             }
         case ItemType.facility.rawValue:
             if let facilityCatalogModel = catalogModel as? FacilityCatalogModel {
-                CfEComCatalog().updateFacilityCatalog(facilityId: itemId, facilityCatalogModel: facilityCatalogModel)
+                CfEComCatalog.updateFacilityCatalog(facilityId: itemId, facilityCatalogModel: facilityCatalogModel)
             }
         default:
             ExceptionManager.throwIllegalStateException(eventType:catalogName, message:"Please use correct catalog properties with provided item type", className:String(describing: CfEComCatalog.self))
@@ -56,61 +56,61 @@ public class CfEComCatalog {
         }
         
         // Grocery Catalog
-        public func updateGroceryCatalog(itemId: String, groceryCatalogModel: String) {
+        public static func updateGroceryCatalog(itemId: String, groceryCatalogModel: String) {
             if let groceryCatalogModel = try? JSONDecoder().decode(GroceryCatalogModel.self, from: Data(groceryCatalogModel.utf8)) {
                 updateGroceryCatalog(itemId: itemId, groceryCatalogModel: groceryCatalogModel)
             }
         }
         
-        public func updateGroceryCatalog(itemId: String, groceryCatalogModel: GroceryCatalogModel) {
+        public static func updateGroceryCatalog(itemId: String, groceryCatalogModel: GroceryCatalogModel) {
             let groceryInternalCatalog = ECommerceConstants.verifyCatalogForGrocery(itemId: itemId, groceryCatalogModel: groceryCatalogModel)
             CFSetup().updateEcommerceCatalogItem(subject: .grocery, catalogObject: [groceryInternalCatalog].toData()!)
         }
         
         // Blood Catalog
-        public func updateBloodCatalog(itemId: String, bloodCatalogModel: String) {
+        public static func updateBloodCatalog(itemId: String, bloodCatalogModel: String) {
             if let bloodCatalogModel = try? JSONDecoder().decode(BloodCatalogModel.self, from: Data(bloodCatalogModel.utf8)) {
                 updateBloodCatalog(itemId: itemId, bloodCatalogModel: bloodCatalogModel)
             }
         }
         
-        public func updateBloodCatalog(itemId: String, bloodCatalogModel: BloodCatalogModel) {
+        public static func updateBloodCatalog(itemId: String, bloodCatalogModel: BloodCatalogModel) {
             let bloodInternalCatalog = ECommerceConstants.verifyCatalogForBlood(itemId: itemId, bloodCatalogModel: bloodCatalogModel)
             CFSetup().updateEcommerceCatalogItem(subject: .blood, catalogObject: [bloodInternalCatalog].toData()!)
         }
         
         // Oxygen Catalog
-        public func updateOxygenCatalog(itemId: String, oxygenCatalogModel: String) {
+        public static  func updateOxygenCatalog(itemId: String, oxygenCatalogModel: String) {
             if let oxygenCatalogModel = try? JSONDecoder().decode(OxygenCatalogModel.self, from: Data(oxygenCatalogModel.utf8)) {
                 updateOxygenCatalog(itemId: itemId, oxygenCatalogModel: oxygenCatalogModel)
             }
         }
         
-        public func updateOxygenCatalog(itemId: String, oxygenCatalogModel: OxygenCatalogModel) {
+        public static func updateOxygenCatalog(itemId: String, oxygenCatalogModel: OxygenCatalogModel) {
             let oxygenInternalCatalog = ECommerceConstants.verifyCatalogForOxygen(itemId: itemId, oxygenCatalogModel: oxygenCatalogModel)
             CFSetup().updateEcommerceCatalogItem(subject: .oxygen, catalogObject: [oxygenInternalCatalog].toData()!)
         }
         
         // Medical Equipment Catalog
-        public func updateMedicalEquipmentCatalog(itemId: String, medicalEquipmentCatalogModel: String) {
+        public static func updateMedicalEquipmentCatalog(itemId: String, medicalEquipmentCatalogModel: String) {
             if let medicalEquipmentCatalogModel = try? JSONDecoder().decode(MedicalEquipmentCatalogModel.self, from: Data(medicalEquipmentCatalogModel.utf8)) {
                 updateMedicalEquipmentCatalog(itemId: itemId, medicalEquipmentCatalogModel: medicalEquipmentCatalogModel)
             }
         }
         
-        public func updateMedicalEquipmentCatalog(itemId: String, medicalEquipmentCatalogModel: MedicalEquipmentCatalogModel) {
+        public static func updateMedicalEquipmentCatalog(itemId: String, medicalEquipmentCatalogModel: MedicalEquipmentCatalogModel) {
             let medicalInternalCatalog = ECommerceConstants.verifyCatalogForMedicalEquipment(itemId: itemId, medicalEquipmentCatalogModel: medicalEquipmentCatalogModel)
             CFSetup().updateEcommerceCatalogItem(subject: .medical_equipment, catalogObject: [medicalInternalCatalog].toData()!)
         }
         
         // Facility Catalogd
-        public func updateFacilityCatalog(facilityId: String, facilityCatalogModel: String) {
+        public static func updateFacilityCatalog(facilityId: String, facilityCatalogModel: String) {
             if let facilityCatalogModel = try? JSONDecoder().decode(FacilityCatalogModel.self, from: Data(facilityCatalogModel.utf8)) {
                 updateFacilityCatalog(facilityId: facilityId, facilityCatalogModel: facilityCatalogModel)
             }
         }
         
-        public func updateFacilityCatalog(facilityId: String, facilityCatalogModel: FacilityCatalogModel) {
+        public static func updateFacilityCatalog(facilityId: String, facilityCatalogModel: FacilityCatalogModel) {
             let facilityInternalCatalog = ECommerceConstants.verifyCatalogForFacility(facilityId: facilityId, facilityCatalogModel: facilityCatalogModel)
             CFSetup().updateEcommerceCatalogItem(subject: .facility, catalogObject: [facilityInternalCatalog].toData()!)
         }
