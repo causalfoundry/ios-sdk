8347e763947b9799560e7dce73ed6152ee1a07bf
Bastian Kohlbauer <bastian@bastian.codes>
2023-11-30 14:39:40 +0100

    fix duration and render time log

--- Core/Sources/View Controllers/CFViewController.swift
+++ Core/Sources/View Controllers/CFViewController.swift
@@ -17,18 +17,36 @@ open class CFViewController: UIViewController {
         return "\(Bundle.main.bundleIdentifier ?? "")/\(self.className)"
     }
     
+    private var renderBeginTime: CFAbsoluteTime!
+    private var renderEndTime: CFAbsoluteTime!
+    private var durationBeginTime: CFAbsoluteTime!
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        renderBeginTime = CFAbsoluteTimeGetCurrent()
+    }
+    
     open override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        durationBeginTime = CFAbsoluteTimeGetCurrent()
+        if renderEndTime != nil {
+            // only set once. user can return to preview view in stack
+            renderEndTime = CFAbsoluteTimeGetCurrent()
+        }
+    }
+    
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         CfLogPageBuilder()
             .setContentBlock(content_block: CoreConstants.shared.contentBlock)
             .setTitle(title: self.className)
             .setPath(path: self.path)
-            .setDuration(duration: 30)
-            .setRenderTime(render_time: 300)
+            .setDuration(duration: Float(CFAbsoluteTimeGetCurrent() - durationBeginTime))
+            .setRenderTime(render_time: Int(renderEndTime - renderBeginTime))
             .build()
-    }
-    
-    open override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
     }
 }
