535498ae31fd5ed68f8382818596fb67ce15c227
moizhassankh <62547067+moizhassankh@users.noreply.github.com>
2023-12-06 20:05:13 +0100

    ♻️ refactor: delivery event

--- E_Commerce/Sources/Models/event_Objetcs/DeliveryObject.swift
+++ E_Commerce/Sources/Models/event_Objetcs/DeliveryObject.swift
@@ -2,54 +2,55 @@
 //  DeliveryObject.swift
 //
 //
-//  Created by khushbu on 29/10/23.
+//  Created by moizhassankh on 06/12/23.
 //
 
 import Foundation
 
-import Foundation
-
 public struct DeliveryObject: Codable {
-    var id: String
-    var order_id: String
+    var deliveryId: String
+    var orderId: String
     var action: String
     var meta: Encodable?
 
     enum CodingKeys: String, CodingKey {
-        case id
-        case order_id
+        case deliveryId = "id"
+        case orderId = "order_id"
         case action
         case meta
     }
 
-    public init(id: String, order_id: String, action: String, meta: Encodable? = nil) {
-        self.id = id
-        self.order_id = order_id
+    public init(deliveryId: String, orderId: String, action: String, meta: Encodable? = nil) {
+        self.deliveryId = deliveryId
+        self.orderId = orderId
         self.action = action
         self.meta = meta
     }
 
     // MARK: - Codable
 
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(String.self, forKey: .id)
-        order_id = try container.decode(String.self, forKey: .order_id)
-        action = try container.decode(String.self, forKey: .action)
-        if let metaData = try container.decodeIfPresent(Data.self, forKey: .meta) {
-            meta = try? (JSONSerialization.jsonObject(with: metaData, options: .allowFragments) as! any Encodable)
-        } else {
-            meta = nil
-        }
-    }
-
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(order_id, forKey: .order_id)
+        try container.encode(deliveryId, forKey: .deliveryId)
+        try container.encode(orderId, forKey: .orderId)
         try container.encode(action, forKey: .action)
         if let metaData = meta {
             try container.encode(metaData, forKey: .meta)
         }
+        
+    }
+
+    // Custom decoding method
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        deliveryId = try container.decode(String.self, forKey: .deliveryId)
+        orderId = try container.decode(String.self, forKey: .orderId)
+        action = try container.decode(String.self, forKey: .action)
+        if let metaData = try? container.decodeIfPresent(Data.self, forKey: .meta) {
+            meta = try? (JSONSerialization.jsonObject(with: metaData, options: .allowFragments) as! any Encodable)
+        } else {
+            meta = nil
+        }
+        
     }
 }
