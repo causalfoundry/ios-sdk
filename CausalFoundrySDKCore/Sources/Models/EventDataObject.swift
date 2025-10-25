c8aea281e87a1c2a23b1ccbf23341f2cf7781c93
moizhassankh <62547067+moizhassankh@users.noreply.github.com>
2025-10-25 22:58:27 +0200

    🚧 wip

--- CausalFoundrySDKCore/Sources/Models/EventDataObject.swift
+++ CausalFoundrySDKCore/Sources/Models/EventDataObject.swift
@@ -9,30 +9,30 @@ import Foundation
 
 struct EventDataObject: Codable, Hashable {
     enum CodingKeys: String, CodingKey {
-        case ol
-        case ts
+        case ts = "time"
         case name
         case property
         case ctx
     }
 
-    let ol: Bool
     let ts: String
     let name: String
     let property: String
     let ctx: [String: Any]?
 
     init<T: Codable>(ol: Bool, ts: Date, name: String, property: String, ctx: T) {
-        self.ol = ol
         self.ts = ISO8601DateFormatter().string(from: ts)
         self.name = name
         self.property = property
-        self.ctx = ctx.serializeToFlatMap()
+        // Serialize ctx to flat map
+          var flatMap = ctx.serializeToFlatMap()
+          // Add "__ol" entry
+          flatMap["__ol"] = ol
+          self.ctx = flatMap
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        ol = try container.decode(Bool.self, forKey: .ol)
         ts = try container.decode(String.self, forKey: .ts)
         name = try container.decode(String.self, forKey: .name)
         property = try container.decode(String.self, forKey: .property)
@@ -41,7 +41,6 @@ struct EventDataObject: Codable, Hashable {
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(ol, forKey: .ol)
         try container.encode(ts, forKey: .ts)
         try container.encode(name, forKey: .name)
         try container.encode(property, forKey: .property)
@@ -56,7 +55,6 @@ struct EventDataObject: Codable, Hashable {
     // Hashable conformance with props inclusion
     func hash(into hasher: inout Hasher) {
         hasher.combine(name)
-        hasher.combine(ol)
         hasher.combine(ts)
         
         if let props = propsAsJSONData() {
@@ -66,7 +64,6 @@ struct EventDataObject: Codable, Hashable {
 
     static func == (lhs: EventDataObject, rhs: EventDataObject) -> Bool {
         return lhs.name == rhs.name &&
-               lhs.ol == rhs.ol &&
                lhs.ts == rhs.ts &&
                lhs.property == rhs.property &&
                lhs.propsAsJSONData() == rhs.propsAsJSONData()
